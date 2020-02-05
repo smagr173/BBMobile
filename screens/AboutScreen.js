0d65d6497816c3af2cb5b9e75ef15dbdb160c0f2
smagr173 <54822866+smagr173@users.noreply.github.com>
2020-02-05 16:02:15 -0500

    Add files via upload

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -3,12 +3,17 @@
 /*  Created:    January 25, 2020                                    */
 /*  Course:     CSC 355-020                                         */
 /*  Professor:  Dr. Tan                                             */
-/*  Filename:   Information.js                                      */
-/*  Purpose:    This file contains Information about the bagle shop */
+/*  Filename:   AboutScreen.js                                      */
+/*  Purpose:    This file contains the user registration screen     */
+/*              to create an account. It allows the user to         */
+/*              enter their name, email, and password into the      */
+/*              input fields. These credentials are then sent to    */
+/*              a PHP file where they are entered into a database.  */
+/*                                                                  */
 /********************************************************************/
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Button, TextInput, TouchableOpacity,Image } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default class HomeScreen extends Component {
   static navigationOptions = {
@@ -16,82 +21,59 @@ export default class HomeScreen extends Component {
  };
 
 render() {
- // const {navigate} = this.props.navigation;
   return (
-  <View style={styles.container}>
-
-  <Image source={{uri: 'http://csitrd.kutztown.edu/~smagr173/MainLogo.png'}}
-	 style={styles.image1} />
-  <Text style={styles.divider}>_____________________________</Text>
-  <Text style={styles.spacingForHeadder}> </Text>
-  <Text style={styles.spacingForHeadder}> </Text>
-
+    <View style={styles.container}>
   
-  <Text style={styles.subtitle}>Address</Text>
-  <Text style={styles.bodyText}>214 W. Main St. Kutztown, PA 19530</Text>
-  <Text style={styles.spacingForHeadder}> </Text>
-  <Text style={styles.subtitle}>Contact</Text>
-  <Text style={styles.bodyText}>610-683-5440</Text>
-  <Text style={styles.spacingForHeadder}> </Text>
-  <Text style={styles.subtitle}>Hours of Operation</Text>
-  <Text style={styles.bodyText}>Weekdays: 8:00 AM - 3:00 PM</Text>
-  <Text style={styles.bodyText}>Weekends: 7:00 AM - 3:00 PM</Text>
-  <Text style={styles.spacingForHeadder}> </Text>
-  <Text style={styles.spacingForHeadder}> </Text>  
-  </View>
+    <Image source={{uri: 'http://csitrd.kutztown.edu/~smagr173/MainLogo.png'}}
+     style={styles.image1} />
+    <Text style={styles.divider}>_____________________________</Text>
+    
+    <Text style={styles.subtitle}>Address</Text>
+    <Text style={styles.bodyText}>214 W. Main St. Kutztown, PA 19530</Text>
 
- );
+    <Text style={styles.subtitle}>Contact</Text>
+    <Text style={styles.bodyText}>610-683-5440</Text>
+ 
+    <Text style={styles.subtitle}>Hours of Operation</Text>
+    <Text style={styles.bodyText}>Weekdays: 8:00 AM - 3:00 PM</Text>
+    <Text style={styles.bodyText}>Weekends: 7:00 AM - 3:00 PM</Text>
+    </View>
+  
+   );
 }
 }
 
 const styles = StyleSheet.create({
-
-  container: {
+container: {
   flex: 1,
   justifyContent: 'center',
   alignItems: 'center',
-  backgroundColor: '#F5FCFF',
+  backgroundColor: 'white',
 },
-
 bodyText:{
   color:'gray',
   textAlign:'center',
-  fontSize:20
+  fontSize:20,
+  marginBottom:5,
 },
-
-title:{
-  
-  fontWeight:'bold',
-  color:'gray',
-  textAlign:'center',
-  fontSize:30
-
-},
-
 subtitle:{
-  
   fontWeight:'bold',
   color:'gray',
   textAlign:'center',
-  fontSize:25
+  fontSize:25,
+  marginBottom: 10,
 
 },
-
 image1: {
-  width:360,
-  height:210
+  width:330,
+  height:190,
 },
-
 divider: {
+  marginTop: 10,
   fontWeight:'bold',
   color:'black',
   textAlign:'center',
   fontSize:17,
-  
+  marginBottom: 40
 },
-
-spacingForHeadder:{
-marginBottom: 25
-},
-
 });
