67ee2b7b831c0cae63bb75733784869a5e883d73
TheRealTykur <53436633+TheRealTykur@users.noreply.github.com>
2020-02-05 12:17:44 -0500

    Add files via upload

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,23 +1,18 @@
 /********************************************************************/
-/*  Author:     Stephen Magrowski                                   */
+/*  Author:     Tyler Kurzawa                                       */
 /*  Created:    January 25, 2020                                    */
 /*  Course:     CSC 355-020                                         */
 /*  Professor:  Dr. Tan                                             */
-/*  Filename:   AboutScreen.js                                      */
-/*  Purpose:    This file contains the user registration screen     */
-/*              to create an account. It allows the user to         */
-/*              enter their name, email, and password into the      */
-/*              input fields. These credentials are then sent to    */
-/*              a PHP file where they are entered into a database.  */
-/*                                                                  */
+/*  Filename:   Information.js                                      */
+/*  Purpose:    This file contains Information about the bagle shop */
 /********************************************************************/
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Button, TextInput, TouchableOpacity,Image } from 'react-native';
 
 export default class HomeScreen extends Component {
   static navigationOptions = {
-    title: 'About',
+    title: 'Information',
  };
 
 render() {
@@ -25,36 +20,78 @@ render() {
   return (
   <View style={styles.container}>
 
-<Text style={styles.pageText}>Location & Contact</Text>
+  <Image source={{uri: 'http://csitrd.kutztown.edu/~smagr173/MainLogo.png'}}
+	 style={styles.image1} />
+  <Text style={styles.divider}>_____________________________</Text>
+  <Text style={styles.spacingForHeadder}> </Text>
+  <Text style={styles.spacingForHeadder}> </Text>
 
-   </View>
+  
+  <Text style={styles.subtitle}>Address</Text>
+  <Text style={styles.bodyText}>214 W. Main St. Kutztown, PA 19530</Text>
+  <Text style={styles.spacingForHeadder}> </Text>
+  <Text style={styles.subtitle}>Contact</Text>
+  <Text style={styles.bodyText}>610-683-5440</Text>
+  <Text style={styles.spacingForHeadder}> </Text>
+  <Text style={styles.subtitle}>Hours of Operation</Text>
+  <Text style={styles.bodyText}>Weekdays: 8:00 AM - 3:00 PM</Text>
+  <Text style={styles.bodyText}>Weekends: 7:00 AM - 3:00 PM</Text>
+  <Text style={styles.spacingForHeadder}> </Text>
+  <Text style={styles.spacingForHeadder}> </Text>  
+  </View>
 
  );
 }
 }
 
 const styles = StyleSheet.create({
-container: {
+
+  container: {
   flex: 1,
   justifyContent: 'center',
   alignItems: 'center',
   backgroundColor: '#F5FCFF',
 },
-welcome: {
-  fontSize: 20,
-  textAlign: 'center',
-  margin: 10,
+
+bodyText:{
+  color:'gray',
+  textAlign:'center',
+  fontSize:20
 },
-instructions: {
-  textAlign: 'center',
-  color: '#333333',
-  marginBottom: 5,
-},
-pageText: {
-  margin:10,
+
+title:{
+  
   fontWeight:'bold',
   color:'gray',
   textAlign:'center',
-  fontSize:15
-}
+  fontSize:30
+
+},
+
+subtitle:{
+  
+  fontWeight:'bold',
+  color:'gray',
+  textAlign:'center',
+  fontSize:25
+
+},
+
+image1: {
+  width:360,
+  height:210
+},
+
+divider: {
+  fontWeight:'bold',
+  color:'black',
+  textAlign:'center',
+  fontSize:17,
+  
+},
+
+spacingForHeadder:{
+marginBottom: 25
+},
+
 });
