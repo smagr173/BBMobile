1b26154412b026b4b304700eb4cde7c591e9e548
Stephen Magrowski <54822866+smagr173@users.noreply.github.com>
2020-02-24 00:57:27 -0500

    Add files via upload

--- PHP_Backend/fetchRecord.php
+++ PHP_Backend/fetchRecord.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
 
+if (isset($_SESSION['record'])) {
 // Fetch user record
 $record = $_SESSION['record'];
+
 $email = $record['email'];
 $fName = $record['fname'];
 $lName = $record['lname'];
@@ -10,6 +12,10 @@ $lName = $record['lname'];
 $msg['fname'] = $fName;
 $msg['lname'] = $lName;
 $msg['email'] = $email;
+}
+else {
+	$msg['empty'] = 0;
+}
 
 echo json_encode($msg);  // Display messages in JSON format
 ?>
